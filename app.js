f953619ef1888d1521948129350ca8c0b8cc22a6
Patricio Frascaroli <pmfrascaroli@gmail.com>
2023-07-18 15:03:17 -0300

    update css

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
-const express = require('express');
+const express = require("express");
+const path = require("path");
+
 const app = express();
-const path = require('path');
 const PORT = 3030;
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('public'));
 
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'views', 'home.html')));
+/* rutas */
+app.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "views", "home.html"))
+);
+app.get("/header", (req, res) =>
+  res.sendFile(path.join(__dirname, "views","partials", "header.html"))
+);
 
-app.listen(PORT, () => console.log('Servidor corriendo en http://localhost:' + PORT));
+
+app.listen(PORT, () =>
+  console.log("Server running in http://localhost:" + PORT)
+);
